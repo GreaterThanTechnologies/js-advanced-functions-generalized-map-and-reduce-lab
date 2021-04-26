deaaf0e964360ff73ba15608d198e00e71f81c02
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-04-26 17:01:46 -0400

    commit tree 75e15a1d13e77f36e37647c25c1305b09677c102

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+
+function map(src, callb) {
+  let r = [];
+  for (let i = 0; i < src.length; i++) {
+    let theElement = src[i];
+    r.push(callb(theElement));
+  }
+  return r
+}
+
+function reduce(src, callb, strtValue) {
+  let total;
+  if (strtValue) {
+    total = strtValue;
+    for (let i = 0; i < src.length; i++) {
+      total = callb(src[i], total);
+    }
+    return total;
+  } else {
+      total = src[0];
+      for (let i = 1; i < src.length; i++) {
+        total = callb(src[i], total);
+      }
+      return total;
+  }
+}
